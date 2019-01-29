1deb412ea2a75fd0dcfd268a02ca1a5f18b86ef8
李然 <liran@iermu.com>
2019-01-29 15:49:00 +0800

    up

--- app/public/pages/data-minzhengt/page.js
+++ app/public/pages/data-minzhengt/page.js
@@ -1,7 +1,7 @@
 require.config(APP_CONFIG);
 
 require(["init", "components/s-pro-top-nav/component",
-  "pages/data-statistics/controller/controller",
+  "pages/data-minzhengt/controller/controller",
   "app"
 ], function(ZBase) {
   ZBase.init();
