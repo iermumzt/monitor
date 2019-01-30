075e05434f92638751480b5ea5efedb58fa0e762
李然 <liran@iermu.com>
2019-01-30 10:48:42 +0800

    up

--- app/public/pages/data-minzhengt/route.js
+++ app/public/pages/data-minzhengt/route.js
@@ -1,7 +1,7 @@
 //let videoTpl = require(process.cwd() + "/lib/video_loading_tpl");
 module.exports = {
   bodyClass: "data-statistics",
-  layout: "_layout10",
+  layout: "_layout10_mzt",
   // fn(req, res, next) {
   //  //  res.template.page_config.zhibotpl = videoTpl.getTpl(7, null, req.cookies.language);
   //   // res.template.page_config.zhibotpl2 = videoTpl.getTpl(20, null, req.cookies.language);
